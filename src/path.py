22c661a567b64af704dcbcce56337c395925e6ce
Widmark Kaue Silva Cardoso <widmark.kaue@grad.ufsc.br>
2023-04-09 19:55:59 -0300

    update nome do repositório

--- src/path.py
+++ src/path.py
@@ -2,11 +2,11 @@
 from pathlib import Path
 #%% Caminhos Importantes do repositório
 if not Path('/workspaces') in Path().cwd().parents:
-    for path in Path.home().glob('**/Aeroacoustics-Analysis-of-Resonators'):
+    for path in Path.home().glob('**/Aeroacoustics-Resonators'):
         PATH_DATA = path.joinpath('data')
         break
 else:
-    for path in Path.home().parent.with_name('workspaces').glob('Aeroacoustics-Analysis-of-Resonators'):
+    for path in Path.home().parent.with_name('workspaces').glob('Aeroacoustics-Resonators'):
         PATH_DATA = path.joinpath('data')
         break
 PATH_DATA.mkdir(exist_ok=True) 
