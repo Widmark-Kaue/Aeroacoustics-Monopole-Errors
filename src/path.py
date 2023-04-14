a11143345b312547ff090a9e1094dad0e6b10a5c
Widmark Kauê <66392718+Widmark-Kaue@users.noreply.github.com>
2023-04-14 14:11:14 +0000

    adicionado a pasta de probes

--- src/path.py
+++ src/path.py
@@ -3,10 +3,13 @@ from pathlib import Path
 #%% Caminhos Importantes do repositório
 if not Path('/workspaces') in Path().cwd().parents:
     for path in Path.home().glob('**/Aeroacoustics-Resonators'):
+        PATH_PROBES = path.joinpath('observers')
         PATH_DATA = path.joinpath('data')
         break
 else:
     for path in Path.home().parent.with_name('workspaces').glob('Aeroacoustics-Resonators'):
         PATH_DATA = path.joinpath('data')
         break
-PATH_DATA.mkdir(exist_ok=True) 
+
+PATH_PROBES.mkdir(exist_ok=True)
+PATH_DATA.mkdir(exist_ok=True)
