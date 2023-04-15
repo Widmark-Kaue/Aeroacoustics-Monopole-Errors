35705a6d7ab6bcbbab1e1f4e06e1495a80d98910
Widmark Kaue Silva Cardoso <widmark.kaue@grad.ufsc.br>
2023-04-15 10:44:44 -0300

    modificações nas funções das bibliotecas

--- src/observers.py
+++ src/observers.py
@@ -48,8 +48,8 @@ def microphones(
 
 
 #%% Run
-probes(
-    number_of_probes=40_000, lim=(-100, 100), name_of_archive='probesTimeFlow'
-)
+# probes(
+#     number_of_probes=40_000, lim=(-100, 100), name_of_archive='probesTimeFlow'
+# )
 # microphones(number_of_observer=51)
 # %%
--- src/utils.py
+++ src/utils.py
@@ -2,16 +2,16 @@ from numpy import loadtxt, array
 from src.path import PATH_DATA
 
 def importData(
-        simulation: str,
         case: str,
         test: str,
         time: float = 2,
+        simulation: str = 'monopoleFlow' ,
 )-> dict:
 
     toPa     = 101325 
     pressure = {}
 
-    PATH_IMPORT = PATH_DATA.joinpath('monopoleFlow', case, test, time)
+    PATH_IMPORT = PATH_DATA.joinpath(simulation, case, test, str(time))
     for arq in PATH_IMPORT.iterdir():
         p  = loadtxt(arq, comments='#')
         name = arq.stem.split('_')[-1]
