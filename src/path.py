3cd6d8e317280886a27707cba701e6c7b0e0e3c4
Widmark Kaue Silva Cardoso <widmark.kaue@grad.ufsc.br>
2023-04-22 16:01:58 -0300

    busca de repositório atualizada

--- src/path.py
+++ src/path.py
@@ -3,11 +3,10 @@ from pathlib import Path
 #%% Caminhos Importantes do repositório
 path      = Path().absolute()
 dir       = 'Aeroacoustics-Resonators' 
-condition = any(i.name == dir for i in path.parents[:])
-
-assert condition, "Diretório não existe"
 
 if not(path.name == dir): 
+    condition = any(i.name == dir for i in path.parents[:])
+    assert condition, "Diretório não existe"
     for i in range(len(path.parents)):
         if path.parents[i].name == dir:
             path = path.parents[i]
