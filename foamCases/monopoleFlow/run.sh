3ac7df23a6f41a7d4ca357d505e8bc0538ce2935
Widmark <widmarkkaue.s.c@gmail.com>
2023-08-13 23:26:12 -0300

    refactor: atualize setup config

--- foamCases/monopoleFlow/run.sh
+++ foamCases/monopoleFlow/run.sh
@@ -1,23 +1,29 @@
 #!/bin/bash
 #
-# Script de exemplo para submeter trabalho que não use MPI
+# Script de exemplo para submeter trabalho que não use MPI 
 #
-#SBATCH --job-name=NM1            # Nome do trabalho a ser executado (para melhor identificação)
-#SBATCH --partition=open_cpu                  # Em qual fila o trabalho será executado (ver filas disponíveis com o comando sinfo)
-#SBATCH --tasks-per-node=20
-#SBATCH --nodes=1              # Número de nós (computadores) que serão utilizados (1 para códigos openMP)
-#SBATCH --time=30-00:00:00                    # Tempo máximo de simulação (D-HH:MM). O tempo 00:00:00 corresponde a sem limite.
+#SBATCH --job-name=nomeDoTrabalho          # Nome do trabalho a ser executado (para melhor identificação)
+#SBATCH --partition=open_cpu             # Em qual fila o trabalho será executado (ver filas disponíveis com o comando sinfo)
+#SBATCH --nodes 1                          # Número de nós (computadores) que serão utilizados (1 para códigos openMP)
+#SBATCH --cpus-per-task=20                  # Número de cores que será utilizado
+#SBATCH --mem 1000                         # Quanto de memória em MB por nó (computador) o programa necessitará. 
+#SBATCH --time=30:00:00                    # Tempo máximo de simulação (D-HH:MM). 
 #SBATCH -o slurm.%N.%j.out                 # Nome do arquivo onde a saída (stdout) será gravada %N = Máquina , %j = Número do trabalho. 
-#SBATCH -e slurm.%N.%j.err                    # Nome do arquivo para qual a saída de erros  (stderr) será redirecionada.
-#SBATCH --mail-user=widmark.cardoso@teg.ufsc.br # Email para enviar notificações sobre alteração no estados do trabalho
+#SBATCH -e slurm.%N.%j.err                 # Nome do arquivo para qual a saída de erros  (stderr) será redirecionada.
+#SBATCH --mail-user=widmark.cardoso@teg.ufsc.br            # Email para enviar notificações sobre alteração no estados do trabalho
 #SBATCH --mail-type=BEGIN                  # Envia email quando o trabalho for iniciado
 #SBATCH --mail-type=END                    # Envia email quando o trabalho finalizar
 #SBATCH --mail-type=FAIL                   # Envia email caso o trabalho apresentar uma mensagem de erro.
 
-module load gnu9
-module load openmpi4
-module load fftw
-module load openfoam/2112_Int32
+# Repassa ao OpenMP quantos cores foram definidos nesse script
+if [ -n "$SLURM_CPUS_PER_TASK" ]; then
+	omp_threads=$SLURM_CPUS_PER_TASK
+else
+	omp_threads=1
+fi
+export OMP_NUM_THREADS=$omp_threads
+
+module load openfoam.com/2112
 
 source $WM_PROJECT_DIR/bin/tools/RunFunctions
 
@@ -36,7 +42,7 @@ source $WM_PROJECT_DIR/bin/tools/RunFunctions
 decomposePar > log.decomposePar
 
 #--- Rodar simulação
-runParallel myrhoCentralFoam
+runParallel $(getApplication)
 
 #--- Pós-processamento
 runApplication reconstructPar -latestTime
