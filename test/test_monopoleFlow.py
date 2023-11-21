9685de2a5961927d26e4f0629fd2439c82a4f00c
unknown <WKCARDO@NB55199S.adprd.emb>
2023-11-20 21:57:58 -0300

    test: test new analitic solution for monopole

--- test/test_monopoleFlow.py
+++ test/test_monopoleFlow.py
@@ -1,5 +1,16 @@
 #%% code
-from src.monopole import monopoleFlowSy
+from src.monopole import monopoleFlowSy, monopoleFlowSyE, PATH_DATA
 
-monopoleFlowSy()
+PATH_ANALYTICAL = PATH_DATA.joinpath('monopoleFlow', 'analytical')
+#Teste symengine
+d = 3*42
+monopoleFlowSyE(
+    t           = [2],
+    M           = 0.2,  
+    xlim        = (-d, d),
+    ylim        = (-d, d),
+    nxy         = (2*d+1, 2*d+1),
+    savePath    = PATH_ANALYTICAL,
+    outName     = 'monopole_10Hz_M0.2_teste.json'
+)
 # %%
