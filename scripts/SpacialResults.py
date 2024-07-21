ee8ac6ead49e4249e7f08fff8b9aa6a39c881245
Widmark-Kaue <widmarkkaue.s.c@gmail.com>
2024-07-20 21:09:32 -0300

    refactor: change to get results with util zone with 4.5\lambda

--- scripts/SpacialResults.py
+++ scripts/SpacialResults.py
@@ -126,31 +126,39 @@ fig2.write_image(
     width=1100,
     height=600)
 # %% VARIAÇÃO DOS ESQUEMAS ESPACIAIS
-for ppw in [16, 32]:
+analitic = analitic_mach02.with_name('monopole_10Hz_M02_t02_4s_ZU6.pkl')
+
+for ppw in [32]:
     psimS = importData(
         case = case, 
-        test='circMesh_ZU',
+        test='circMesh_4.5ZU_15ZS',
         time = time, 
-        subcase = 'standardSpacial',
+        subcase = 'standard-SpacialData',
         keyword= f'{ppw}ppw',
         # xsim_range=(-10*42, 10*42, -10*42, 10*42)
-        xsim_range=(-10*42, 10*42, -3*42, 3*42)
+        xsim_range=(-10*42, 10*42, -4.5*42, 4.5*42)
     )
     psimS2 = importData(
         case = case,
-        test='circMesh_ZU',
+        test='circMesh_4.5ZU_15ZS',
         time = time,
-        subcase = 'spacialSchemesTest',
+        subcase = 'spacialSchemes-SpacialData',
         keyword= f'{ppw}ppw',
         # xsim_range=(-10*42, 10*42, -10*42, 10*42)
-        xsim_range=(-10*42, 10*42, -3*42, 3*42)
+        xsim_range=(-10*42, 10*42, -4.5*42, 4.5*42)
     )
 
+    psimS = {**psimS, **psimS2}
+    
+    psimSort = {}
+    for key in sorted(psimS):
+        psimSort[key] = psimS[key]
+
     plotSchemesGO(
-        psim = {**psimS, **psimS2}, 
+        psim = psimSort, 
         # xsim=(-10*42, 10*42), 
-        xsim=(-3*42, 3*42), 
-        analitc=analitic_mach02,
+        xsim=(-4.5*42, 4.5*42), 
+        analitc=analitic,
         time = time, 
         save=True, 
         save_name= f'Spacial_comp_schemes_{ppw}PPW',
@@ -158,6 +166,7 @@ for ppw in [16, 32]:
         windows =True,
         rms_acur= 5, 
         numlegend=1,
+        ticktext=[-4.5, -1.5, 1.5, 4.5],
         plotconfig = dict(
             xaxis = dict(
                 ticks ='outside',
@@ -173,27 +182,35 @@ for ppw in [16, 32]:
     )
 #%% VARIAÇÃO DOS ESQUEMAS TEMPORAIS
 # n = [400, 600, 800, 1000]
-ntime = [400, 800, 1000]
+analitic = analitic_mach02.with_name('monopole_10Hz_M02_t02_4s_ZU6.pkl')
+ntime = [800, 1000]
 for n in ntime:
     psimS2 = importData(
         case = case,
-        test='circMesh_ZU',
+        test='circMesh_4.5ZU_15ZS',
         time = time,
-        subcase = 'timeSchemesTest',
+        subcase = 'timeSchemes-SpacialData',
         keyword= f'n{n}',
-        xsim_range=(-10*42, 10*42, -3*42, 3*42)
+        xsim_range=(-10*42, 10*42, -4.5*42, 4.5*42)
     )
 
-    plotSchemesGO(
-        psim = psimS2, 
-        xsim=(-3*42, 3*42), 
-        analitc=analitic_mach02,
+    psimSort = {}
+    for key in ['crankNicolson', 'Euler', 'backward']:
+        
+        psimSort[key] = psimS2[f'{key} 32ppw n{n}']
+    
+    
+    fig, path = plotSchemesGO(
+        psim = psimSort, 
+        xsim=(-4.5*42, 4.5*42), 
+        analitc=analitic,
         time = time, 
         save=True, 
-        save_name= f'Time_comp_schemes_n{n}',
+        save_name= f'Time_comp_schemes_n{n}_4_5ZU',
         format='pdf',
         windows =True, 
         numlegend=1,
+        ticktext=[-4.5, -1.5, 1.5, 4.5],
         plotconfig = dict(
             xaxis = dict(
                 ticks ='outside',
@@ -208,6 +225,99 @@ for n in ntime:
         )
     )
 
+    fig.write_image(path, format = 'pdf', scale = 5)
+    
+
+# %% VARIAÇÃO DOS ESQUEMAS ESPACIAIS MALHA HÍBRIDA
+psimS2 = importData(
+    case = case,
+    test='hybridMesh',
+    time = time,
+    subcase = 'spacialSchemes-SpacialData',
+    # xsim_range=(-10*42, 10*42, -10*42, 10*42)
+    xsim_range=(-10*42, 10*42, -4.5*42, 4.5*42)
+)
+analitic = analitic_mach02.with_name('monopole_10Hz_M02_t02_4s_ZU6.pkl')
+
+psimSort = {}
+
+for key in sorted(psimS2):
+    psimSort[key] = psimS2[key]
+
+fig, image_path = plotSchemesGO(
+    psim = psimSort, 
+    # xsim=(-10*42, 10*42), 
+    xsim=(-4.5*42, 4.5*42), 
+    analitc=analitic,
+    time = time, 
+    save=True, 
+    save_name= f'Spacial_comp_schemes_{32}PPW_HybridMesh',
+    format='pdf',
+    windows =True,
+    rms_acur= 5, 
+    numlegend=1,
+    ticktext=[-4.5, -1.5, 1.5, 4.5],
+    plotconfig = dict(
+        xaxis = dict(
+            ticks ='outside',
+            linecolor = 'black',
+            mirror =True
+        ),
+        yaxis = dict(
+            ticks ='outside',
+            linecolor = 'black',
+            mirror =True
+        )
+    )
+)
+fig.write_image(image_path, format = 'pdf', scale = 5)
+
+# %% Efeito das zonas de absorção
+psimS2 = importData(
+    case = case,
+    test='unstructured_circMesh_new_DZ',
+    time = time,
+    subcase = 'standard-SpacialData',
+    # xsim_range=(-10*42, 10*42, -10*42, 10*42)
+    xsim_range=(-10*42, 10*42, 3.5*42, 10*42)
+)
+psimS2['Damping function OpenFOAM'] = psimS2.pop('vanLeer 32ppw n4000')
+
+psimS = importData(
+    case = case,
+    test='unstructured_circMesh_standard_DZ',
+    time = time,
+    subcase = 'standard-SpacialData',
+    # xsim_range=(-10*42, 10*42, -10*42, 10*42)
+    xsim_range=(-10*42, 10*42, 3.5*42, 10*42)
+)
+psimS2['Damping Function Modified'] = psimS.pop('vanLeer 32ppw n4000')
+analitic = analitic_mach02.with_name('monopole_10Hz_M02_t02_4s_ZU6.pkl')
+
+
+plotSchemesGO(
+    psim = psimS2, 
+    # xsim=(-10*42, 10*42), 
+    xsim=(3.5*42, 10*42), 
+#    analitc=analitic,
+    time = time, 
+    save=True, 
+    save_name= f'Spacial_comp_damping_{32}PPW_',
+    format='pdf',
+    rms_acur= 5, 
+    plotconfig = dict(
+        xaxis = dict(
+            ticks ='outside',
+            linecolor = 'black',
+            mirror =True
+        ),
+        yaxis = dict(
+            ticks ='outside',
+            linecolor = 'black',
+            mirror =True
+        )
+    )
+)
 
 # %%
 
